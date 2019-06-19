677f468c031c0762dd40ba49c9c33cedb14b8194
abao <445962176@qq.com>
2019-06-19 18:59:52 +0800

    update arning: LF will be replaced by CRLF in .idea/workspace.xml. The file will have its original line endings in your working directory

--- 第二课：鼠标事件-修饰符-键盘修饰符/js/app.js
+++ 第二课：鼠标事件-修饰符-键盘修饰符/js/app.js
@@ -1,32 +1,42 @@
-new Vue({
-    el: '#vue-app',
-    data: {
-        age: 19,
-        X: 0,
-        Y: 0,
-    },
-    methods: {
-
-        add: function (add) {
-            this.age += add
-        },
-        sub: function (sub) {
-            this.age -= sub
-        },
-        // 鼠标移动
-        updateXY: function (event) {
-            // console.log(event)
-            this.X = event.offsetX;
-            this.Y = event.offsetY;
-        },
-
-        Stop: function (event) {
-            event.stopPropagation();
-        },
-
-        alert: function () {
-            alert("跳转。。。")
-        }
-
-    }
-});
+// new Vue({
+//     el: '#vue-app',
+//     data: {
+//         name:'Vue.js',
+//         age: 19,
+//         X: 0,
+//         Y: 0,
+//     },
+//     methods: {
+//
+//         add: function (add) {
+//             this.age += add
+//         },
+//         sub: function (sub) {
+//             this.age -= sub
+//         },
+//         // 鼠标移动
+//         updateXY: function (event) {
+//             // console.log(event)
+//             this.X = event.offsetX;
+//             this.Y = event.offsetY;
+//         },
+//
+//         Stop: function (event) {
+//             event.stopPropagation();
+//         },
+//
+//         alert: function () {
+//             alert("跳转。。。")
+//         },
+//         onName:function () {
+//             console.log('您正在输入')
+//         },
+//         onAge:function () {
+//             console.log('您正在输入')
+//         },
+//         greet:function () {
+//             alert('Hello' + this.name + '!')
+//         }
+//
+//     }
+// });
